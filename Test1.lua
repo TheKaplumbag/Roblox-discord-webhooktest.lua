6b99083f4c625e2b2f0240610d8d4053702c8835
TheKaplumbag <scpmtf4@gmail.com>
2025-05-07 01:00:39 +0300

    Update Test1.lua

--- Test1.lua
+++ Test1.lua
@@ -11,5 +11,13 @@ local response = HttpService:RequestAsync({
 })
       if response.Success then
             print("Code:", response.StatusCode, response.StatusMessage)
-            print("Body: \n:
+            print("Body: \n", response.Body)
+      else
+            warn("Error code: ", response.StatusCode, "Error message: ", response.StatusMessage)
+      end
 end
+local success, result = pcall(request()
+      if not success then
+            warn("Http request failed: " ..message)
+      end
+end)
