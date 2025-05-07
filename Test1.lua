37d22fd59ef8efe719de18f32e4d9b88dbe36f70
TheKaplumbag <scpmtf4@gmail.com>
2025-05-07 18:33:33 +0300

    Update Test1.lua

--- Test1.lua
+++ Test1.lua
@@ -1,13 +1,13 @@
 local HttpService = game:GetService("HttpService")
 local LocalPlayer = game.Players.LocalPlayer
 local function request()
-local response = HttpService:RequestAsync({
-      Url = "https://discord.com/api/webhooks/1369434804646318091/NVCC-G_eBZ4YhCHROOMLdI0BPI9Qzq6clTmNMK8RJ1hmEagpYiTsyWkc4-Xd6Hi6E-0O",
-      Method = "POST",
-      Headers = {
-        ["Content-Type"] = "application/json",
+      local response = HttpService:RequestAsync({
+                  Url = "https://discord.com/api/webhooks/1369434804646318091/NVCC-G_eBZ4YhCHROOMLdI0BPI9Qzq6clTmNMK8RJ1hmEagpYiTsyWkc4-Xd6Hi6E-0O",
+                  Method = "POST",
+                  Headers = {
+                        ["Content-Type"] = "application/json",
       },
-      Body = HttpService:JSONEncode({Username = LocalPlayer.Name }),
+                  Body = HttpService:JSONEncode({Username = LocalPlayer.Name }),
 })
       if response.Success then
             print("Code:", response.StatusCode, response.StatusMessage)
